09fe6f21ff0b3009e8ed8e3b570bd26c0bf1025c
Kyathi <kyathirekha@gmail.com>
2026-09-18 13:38:07 +0530

    Center services mega menu

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -183,8 +183,8 @@ function DropdownPanel({
   }
 
   const servicePanelStyle = {
-    left: "max(16px, calc((100vw - 1280px) / 2))",
-    right: "calc(max(16px, (100vw - 1280px) / 2) + 168px)",
+    left: "calc(max(16px, (100vw - 1280px) / 2) + 24px)",
+    right: "calc(max(16px, (100vw - 1280px) / 2) + 144px)",
     top: "72px",
     width: "auto",
     maxWidth: "1120px",
