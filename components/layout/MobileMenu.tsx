68473bebba961419ee820e1465fd6529e0b0c304
Kyathi <kyathirekha@gmail.com>
2026-09-20 19:27:02 +0530

    Remove flex-1 stretch on mobile nav so drawer hugs content height

--- components/layout/MobileMenu.tsx
+++ components/layout/MobileMenu.tsx
@@ -148,7 +148,7 @@ export function MobileMenu({ isOpen, onClose }: MobileMenuProps) {
 
         {/* Nav */}
         <nav
-          className="mobile-nav-scroll flex flex-1 min-h-0 flex-col overflow-y-auto px-4 pb-8"
+          className="mobile-nav-scroll flex flex-col overflow-y-auto px-4 pb-8"
           style={{ maxHeight: "calc(100dvh - 4rem)", scrollbarWidth: "thin", scrollbarColor: "rgba(255,255,255,0.25) transparent" }}
         >
           {menuGroups.map((group) => {
